81da6ebb0b0f5ed0f173e58e2019ee9be4fe8c45
hhongyeahh <hhongyeahh@gmail.com>
2024-08-11 19:42:11 +0900

    #13 - repository interface 작성
    이걸로 db에 접근

---
+++ project-board/src/main/java/com/project/projectboard/repository/ArticleRepository.java
@@ -0,0 +1,8 @@
+package com.project.projectboard.repository;
+
+import com.project.projectboard.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+//@Repository 스테레오타입 애노테이션 안 붙여도 동작함 -> JPARepository의 구현체인 SimpleRepository에 붙어있음
+//기본적으로 스테레오 애노테이션은 인터페이스가 아닌 구현체에 붙임
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
