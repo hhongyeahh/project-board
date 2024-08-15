7e618ed9b2854ab9c50e0b00b98f8fca9cf2f02f
hhongyeahh <hhongyeahh@gmail.com>
2024-08-16 01:57:42 +0900

    #4 - 잊고 안 넣은 V2 클래스 추가

--- project-board/src/main/java/com/project/projectboard/dto/v2/response/ArticleWithCommentsResponseV2.java
+++ project-board/src/main/java/com/project/projectboard/dto/v2/response/ArticleWithCommentsResponseV2.java
@@ -15,10 +15,10 @@ public record ArticleWithCommentsResponseV2(
         LocalDateTime createdAt,
         String email,
         String nickname,
-        Set<ArticleCommentResponseV1> articleCommentsResponse
+        Set<ArticleCommentResponseV2> articleCommentsResponse
 ) {
 
-    public static ArticleWithCommentsResponseV2 of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponseV1> articleCommentsResponse) {
+    public static ArticleWithCommentsResponseV2 of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponseV2> articleCommentsResponse) {
         return new ArticleWithCommentsResponseV2(id, title, content, hashtag, createdAt, email, nickname, articleCommentsResponse);
 
     }
@@ -38,7 +38,7 @@ public record ArticleWithCommentsResponseV2(
                 dto.userAccountDto().email(),
                 nickname,
                 dto.articleCommentDtos().stream()
-                .map(ArticleCommentResponseV1::from)
+                .map(ArticleCommentResponseV2::from)
                 .collect(Collectors.toCollection(LinkedHashSet::new))
         );
     }
