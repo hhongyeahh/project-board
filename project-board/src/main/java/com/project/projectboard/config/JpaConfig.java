47db98730105af18700bf2b924559560fa744709
hhongyeahh <hhongyeahh@gmail.com>
2024-08-18 21:38:44 +0900

    #34 - 시큐리티 설정과 자바 대응 코드 구현
    인증 정보를 디비로부터 부르는 빈과 패스워드 인코더 빈까지 등록
    영향 받는 부분을 서비스 코드와 컨트롤러에 반영

--- project-board/src/main/java/com/project/projectboard/config/JpaConfig.java
+++ project-board/src/main/java/com/project/projectboard/config/JpaConfig.java
@@ -1,9 +1,13 @@
 package com.project.projectboard.config;
 
+import com.project.projectboard.dto.security.BoardPrincipal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
@@ -12,6 +16,11 @@ import java.util.Optional;
 public class JpaConfig {
     @Bean
     public AuditorAware<String> auditorAware(){ // auditing에서 이름을 넣는 방법
-        return () -> Optional.of("username"); //TODO: 스프링 시큐리티로 인증 기능을 붙이게 될때, 수정하자
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(BoardPrincipal.class::cast)
+                .map(BoardPrincipal::getUsername);
     }
 }
